663aced25327d6650c333fab440c5ddceda2b827
Daniel Melton <danmelt30@gmail.com>
2021-10-07 04:59:20 -0500

    Subscription and health_check endpoints added | Test added

---
+++ README.md
@@ -0,0 +1,17 @@
+# Zero to production 
+    Chapter 3
+    1. Splitting main into modules
+        - configurations
+        - routes/
+        - startup
+    2. Creating subscription and health_check endpoints
+        - Adding into routes folders.
+    3. Connecting to postgres database using Docker
+        - Using Docker for postgres is to me.
+        - Using sqlx for migration and querying.
+        - Sqlx .env file for database_url. 
+    4. Setting up test
+        - testing which I usually do manually.
+        - Creating test would like to see how database roll back is used.
+        - Thing to do
+            - I would like to split the test into different modules.
---
+++ tests/health_check.rs
@@ -0,0 +1,133 @@
+use std::net::TcpListener;
+use sqlx::{PgPool, PgConnection, Connection, Executor};
+use zero2prod::configuration::{get_connection, DatabaseSettings};
+use zero2prod::startup::run;
+use uuid::Uuid;
+
+
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    let mut connection = PgConnection::connect(&config.connectino_string_without_db())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database.");
+
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
+}
+
+
+async fn spawn_app() -> TestApp {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+
+    let port = listener.local_addr().unwrap().port();    
+    let address = format!("http://127.0.0.1:{}", port);
+
+    let mut configuration = get_connection().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+
+    let connection_pool = configure_database(&configuration.database).await;
+
+    let server = run(listener, connection_pool.clone())
+        .expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+
+    TestApp{
+        address,
+        db_pool: connection_pool,
+    }
+
+}
+
+
+#[actix_rt::test]
+async fn health_check_works() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(&format!("{}/health_check", &app.address))
+        .send()
+        .await
+        .expect("Failed to excute request. ");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+ 
+#[actix_rt::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app = spawn_app().await;
+
+
+    let client = reqwest::Client::new();
+    let body = "name=Big%20Dawg&email=danmelt55%40gmail.com";
+
+
+    let response = client
+        .post(&format!("{}/subscriptions", &app.address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failled to execute request");
+
+
+    assert_eq!(200, response.status().as_u16());
+
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "danmelt55@gmail.com");
+    assert_eq!(saved.name, "Big Dawg")
+
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=Big%20Dawg", "missing the email"),
+        ("email=danmelt55%40gmail.com", "missing the name"), 
+        ("", "missing name and email")
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+
+        let response = client
+        .post(&format!("{}/subscriptions", &app.address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(invalid_body)
+        .send()
+        .await
+        .expect("Failled to execute request");
+
+        assert_eq!(
+            400, 
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }    
+}
